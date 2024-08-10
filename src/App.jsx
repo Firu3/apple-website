27600d25aaa8d38dc2982cdf4aa4f50abdfda21b
Firu2 <ixcombo@hotmail.com>
2024-08-10 04:55:37 +0300

    Adding model and 3d iPhone

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Hero/>
       <Highlights/>
       <Model/>
+      
     </main>
   );
 };
---
+++ src/Faisal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Faisal = () => {
+  return (
+    <div>Faisal</div>
+  )
+}
+
+export default Faisal
